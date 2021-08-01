db3dd5b2a2e5df6be0495c8284c968379081532a
ElkinLondono <elkin.londono@zabud.com.co>
2021-08-01 15:42:11 -0500

    new branch and files based

---
+++ racing/src/test/java/com/elkin/kartRock/racing/RacingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elkin.kartRock.racing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
