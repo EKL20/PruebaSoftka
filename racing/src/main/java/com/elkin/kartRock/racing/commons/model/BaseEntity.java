b65616eb96a3ff2066ce870c4346adcb3d610d42
ElkinLondono <elkin.londono@zabud.com.co>
2021-08-01 23:12:41 -0500

    create feature car-CRUD

---
+++ racing/src/main/java/com/elkin/kartRock/racing/commons/model/BaseEntity.java
@@ -0,0 +1,45 @@
+package com.elkin.kartRock.racing.commons.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+//@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = -1011939721218881483L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    protected boolean deleted;
+
+    @Column(name = "created_by", updatable = false)
+    protected String createdBy;
+
+    @CreationTimestamp
+    @Column(name = "created_At", updatable = false)
+    protected LocalDateTime createdAt;
+
+    @Column(name = "updated_by")
+    protected String updatedBy;
+
+    @UpdateTimestamp
+    @Column(name = "updated_At")
+    protected LocalDateTime updatedAt;
+}
